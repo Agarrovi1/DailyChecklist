cbab5a18fcefd05ad77ed749f8d922682efda53b
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-10-14 10:36:30 -0400

    adds file for tableViewCell

---
+++ DailyChecklist/ListCell.swift
@@ -0,0 +1,9 @@
+//
+//  ListCell.swift
+//  DailyChecklist
+//
+//  Created by Angela Garrovillas on 10/14/20.
+//  Copyright © 2020 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
