0658e26ee8e0a0e1ff73c0567c8863cfe7d655ef
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-10-14 10:44:06 -0400

    adds tableview, textfield and button to VC

--- DailyChecklist/ViewController.swift
+++ DailyChecklist/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - UIObjects
+    var listTableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .blue
+        table.register(ListCell.self, forCellReuseIdentifier: "ListCell")
+        return table
+    }()
+    var textBox: UITextField = {
+        let box = UITextField()
+        box.backgroundColor = .gray
+        return box
+    }()
+    var editButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Edit", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
 
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
