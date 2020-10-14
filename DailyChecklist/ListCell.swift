7ba3d6e7f5008ca2417e4a1c56f2b34ec27961fa
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-10-14 10:38:28 -0400

    adds override inits and required func to cell

--- DailyChecklist/ListCell.swift
+++ DailyChecklist/ListCell.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Angela Garrovillas. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ListCell: UITableViewCell {
+    //MARK: - Overrides
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
